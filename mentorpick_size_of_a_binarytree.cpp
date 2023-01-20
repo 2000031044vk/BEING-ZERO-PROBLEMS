1758692a941222c698f33234dec3deeaf8870404
IamV1neel <2000031044@kluniversity.in>
2023-01-20 14:39:22 +0530

    20-01-2023

---
+++ mentorpick_size_of_a_binarytree.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct BTNode{
+	int val;
+	BTNode *left, *right;
+};
+
+int sizeOfTree(BTNode *root)
+{
+    // TODO:  Fill your code here
+    if(root==NULL){
+        
+        return 0;
+    }
+    return sizeOfTree(root -> left) +sizeOfTree(root -> right) + 1;
+}
